4fca56c69e3bc52ce8740d0069434fbe2dc5c2d3
peacemakerbak <peacemakerbak@gmail.com>
2024-05-28 17:20:02 -0400

    save fo rnow.. react review

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome Peacemaker!
-        </p>
+        <Employee />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Employee.js
@@ -0,0 +1,5 @@
+function Employee() {
+    return <h3> Peacemaker is an employee of Melons.</h3>
+}
+
+export default Employee;
